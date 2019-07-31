ff9ba4a50a605e60396285ad7205fff2f0562834
Saad Khalid <saadkhalid90@gmail.com>
2019-07-31 18:03:00 +0500

    General layout without interaction

---
+++ index.js
@@ -0,0 +1,136 @@
+let topoWorld, gallupPoll, projection, path, colScale;
+
+let params = {};
+params.year = 2016;
+params.groupType = 'Overall'
+params.varType = 'OptimismScore'
+
+async function readAndDraw(){
+  topoWorld = await d3.json('worldMap.topojson');
+  gallupPoll = await d3.csv('gallupSum.csv');
+
+  colScale = d3.scaleSequential(d3.interpolatePuOr)
+    	.domain([75, -75]);
+
+  var steps = 11;
+  //Discrete diverging scale
+  color_threshold = d3.scaleThreshold()
+    	.domain(d3.range(-75 + 150/steps, 75, +150/steps) ) //[-.6, -.2, .2, .6]
+  	  .range(d3.schemePuOr[steps].reverse()); //=> 5 colors in an array
+
+  // define a projection that will be used to make the map
+  projection = d3.geoRobinson()
+    .scale(150 * 1.4)
+    .center([-30,23])
+
+  // this path will be used to compute the d feature of each geometrical path in a map
+  path = d3.geoPath().projection(projection);
+
+  // This converts a json into an array each element corresponding to one path
+  let pathWorld = topojson.feature(topoWorld, topoWorld.objects.worldMap).features;
+
+  // get an array of paths without the path of Antarctica
+  let pathWorld_ = pathWorld.filter(d => d.properties.ADMIN != "Antarctica");
+
+  let SVG = d3.select('svg.GallupPollInd')
+
+  let pathWData = joinSumWithMap(pathWorld_, gallupPoll);
+
+  draw(SVG, pathWData, params);
+
+}
+
+function draw(selection, data, params){
+  selection.append('g')
+    .attr('class', 'Map')
+    .selectAll('path.country')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .call(setFill, color_threshold, params.year, params.groupType, params.varType)
+    .style('stroke', '#212121')
+    .style('stroke-width', '0.5px');
+
+  let legendGroup = selection.append('g')
+          .attr('transform', 'translate(20, 600)')
+          .attr('class', 'legendContainer');
+
+  let legendComp = legendGroup.selectAll('g.legendComponent')
+            .data(d3.range(-75, 75+1, 75/5))
+            .enter()
+            .append('g')
+            .attr('class', 'legendComponent')
+
+  legendComp.append('rect')
+            .attr('width', 25)
+            .attr('height', 10)
+            .attr('transform', (d, i) => {
+              return `translate(${i*25}, 0)`
+            })
+            .style('fill', d => color_threshold(d))
+            // .style('stroke', 'black')
+            // .style('stroke-width', '0.5px');
+
+  legendComp.append('text')
+            .text(d => Math.round(d))
+            .attr('transform', (d, i) => {
+              return `translate(${i*25 + 25/2}, -5)`
+            })
+            .style('text-anchor', ' middle')
+            .style('fill', d => [-75, 75, 0].includes(d) ? 'black': 'none');
+}
+
+function drawUpdate(selection, params){
+  //selection.style('fill', 'none')
+  selection.transition()
+    .duration(1000)
+    .call(setFill, color_threshold, params.year, params.groupType, params.varType);
+}
+
+function setFill(selection, colScaleVar, year, groupType, varType){
+  selection.style('fill', d => {
+    if (d.Gallup){
+      let filtData = d.Gallup.filter(d => d.Year == year & d.groupType == groupType);
+      if (filtData.length > 0){
+        //return colScale(+filtData[0][varType]);
+        return colScaleVar(+filtData[0][varType]);
+      }
+      else {
+        return 'none';
+      }
+    }
+    else {
+      return 'none';
+    }
+  })
+}
+
+readAndDraw();
+
+
+function joinSumWithMap(pathData, GallupData){
+  pathData.forEach(pathCountry => {
+    let countryCode = pathCountry.properties.ADM0_A3;
+    let GallupDatCty = GallupData.filter(d => d.code == countryCode);
+
+    if (GallupDatCty.length > 0){
+      pathCountry.Gallup = GallupDatCty;
+    }
+  })
+  return pathData;
+
+}
+
+d3.selectAll('.selector').on('input', function(d, i){
+  let year = d3.select('.year.selector').node().value;
+  let question = d3.select('.question.selector').node().value;
+
+  let params = {};
+  params.year = year;
+  params.groupType = 'Overall';
+  params.varType = question;
+
+  drawUpdate(d3.selectAll('path'), params);
+})
